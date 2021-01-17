78ba1ab44ebca8a744bff940dc5066bfe0bed664
dennnis0204 <dennnis0204@gmail.com>
2021-01-17 19:41:16 +0100

    Initial commit

---
+++ src/main/java/com/greenparkingbook/desiredevparkingmap/DesiredEvParkingMapApplication.java
@@ -0,0 +1,13 @@
+package com.greenparkingbook.desiredevparkingmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesiredEvParkingMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesiredEvParkingMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenparkingbook/desiredevparkingmap/DesiredEvParkingMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenparkingbook.desiredevparkingmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesiredEvParkingMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
